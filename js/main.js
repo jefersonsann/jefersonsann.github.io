b7448c022dc3ad4c05f2a1d61dc2f1b06d5780f4
Jeferson Santos <email@gmail.com>
2022-07-28 18:49:32 -0300

    Add Animaçoes e Skills

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
-const sections = document.querySelectorAll('.js-scroll');
-const metadeScroll = window.innerHeight * 1.2;
+const anima = document.querySelectorAll('.js-scroll');
+// const metadeScroll = window.innerHeight * 0;
 
 function animaScroll() {
-  sections.forEach((section) => {
-    const sectionTop = section.getBoundingClientRect().top;
-    const isSectionVisible = sectionTop - metadeScroll < 0;
+  anima.forEach((event) => {
+    const sectionTop = event.getBoundingClientRect().top;
+    const isSectionVisible = sectionTop - window.innerHeight < 0;
     if (isSectionVisible) {
-      section.classList.add('ativo');
+      event.classList.add('ativo');
     } else {
-      section.classList.remove('ativo');
+      event.classList.remove('ativo');
     }
   });
 }
