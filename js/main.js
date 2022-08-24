ebdd491eee7f9c26312b059c217ecb913325c2be
Jeferson Santos <jefersonsann@hotmail.com>
2022-08-24 02:05:15 -0300

    Menu adicionado e btn toggle mais efeitos

--- js/main.js
+++ js/main.js
@@ -1,3 +1,30 @@
+// Anima name hover
+const animaName = document.querySelector('#animaName');
+
+function animaNameFunc() {
+  animaName.classList.add('rubberBand');
+  animaName.classList.remove('blast');
+
+  // reset the animaName after a short delay
+  setTimeout(() => {
+    animaName.classList.remove('rubberBand');
+  }, 500);
+  clearTimeout(animaName);
+}
+animaName.addEventListener('mouseover', animaNameFunc);
+
+//  mobile button
+const menu = document.querySelector('#menu');
+const btnMobile = document.querySelector('.mbtn');
+
+function handleMenu(event) {
+  event.preventDefault();
+  btnMobile.classList.toggle('ativo');
+  menu.classList.toggle('ativo');
+}
+btnMobile.addEventListener('click', handleMenu);
+
+// Anima section enter
 const anima = document.querySelectorAll('.js-scroll');
 // const metadeScroll = window.innerHeight * 0;
 
