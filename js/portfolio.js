5f5efcf08081e6e7a31d3820fa9785309219fc4e
Jeferson Santos <jefersonsann@hotmail.com>
2022-09-05 15:11:22 -0300

    Atualização e correção

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,6 +1,4 @@
 export default function Portfolio() {
-  const PortfolioCard = document.querySelector('.portfolio__card');
-
   const fetchPortfolio = async (url) => {
     const res = await fetch(url);
     const portJSON = await res.json();
@@ -11,23 +9,26 @@ export default function Portfolio() {
     });
   };
 
-  const createPortfolio = ({ id, title, image, description }) => {
+  const createPortfolio = ({ id, title, link, image }) => {
     const div = document.createElement('div');
     div.classList.add('portfolio__card');
     div.innerHTML = `
       <div class="portfolio__card--item" key="${id}">
-        ${
-          image &&
-          `
-          <div class="portfolio__card--image">
-            <img src="${image}" alt="${title}" />
+      ${
+        title === 'skeleton'
+          ? '<div class="skeleton"><span>Em brave...</span></div>'
+          : `
+          <a href="${link}" alt="${title}">
+            ${
+              image &&
+              `
+          <div class="portfolio__card--image" style="background-image: url(${image});">
           </div>
         `
-        }          
-        ${
-          description &&
-          `<div class="portfolio__card--desc">${description}</div>`
-        }          
+            }
+          </a>
+        `
+      } 
       </div>
       `;
     return div;
